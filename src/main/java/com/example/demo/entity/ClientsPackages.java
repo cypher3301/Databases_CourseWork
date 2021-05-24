1e19c1618eee5b744caa27d4062b21b9a0221a63
Admin <cypher.is.3301@gmail.com>
2021-05-24 18:13:09 +0300

    entity: ClientsPackages now contains composite id, also has unisertable and non-updatable mappings.

--- src/main/java/com/example/demo/entity/ClientsPackages.java
+++ src/main/java/com/example/demo/entity/ClientsPackages.java
@@ -5,43 +5,66 @@ import org.hibernate.annotations.Immutable;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 
 @Entity(name = "sender_recipient_package")
 @Immutable
 @Data
 @NoArgsConstructor
-@RequiredArgsConstructor
 public class ClientsPackages {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    protected long id;
+    @EmbeddedId
+    protected Id id = new Id();
+
+    @Embeddable
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @EqualsAndHashCode
+    static class Id implements Serializable {
+        @Column(name = "client_sender_id")
+        private long senderId;
+        @Column(name = "client_recipient_id")
+        private long recipientId;
+        @Column(name = "package_id")
+        private long packageId;
+    }
 
     @NotNull
     @NonNull
-    @ManyToOne(cascade = CascadeType.PERSIST)
+    @ManyToOne
     @JoinColumn(
-            name = "client_sender_id",nullable = false
+            name = "client_sender_id",nullable = false,
+            insertable = false,updatable = false
     )
     protected Client sender;
 
     @NotNull
     @NonNull
-    @ManyToOne(cascade = CascadeType.PERSIST)
+    @ManyToOne
     @JoinColumn(
-            name = "client_recipient_id",nullable = false
+            name = "client_recipient_id",nullable = false,
+            insertable = false,updatable = false
     )
     protected Client recipient;
 
     @NotNull
     @NonNull
-    @ManyToOne(cascade = CascadeType.PERSIST)
+    @ManyToOne
     @JoinColumn(
-            name = "package_id",nullable = false
+            name = "package_id",nullable = false,
+            insertable = false,updatable = false
     )
     protected Package packages;
 
+    public ClientsPackages(@NonNull Client sender, @NonNull Client recipient, @NonNull Package packages) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.packages = packages;
+        this.id.packageId= packages.getId();
+        this.id.senderId= sender.getId();
+        this.id.recipientId= recipient.getId();
+    }
 }
 
 
