1ec3c5e5268bc741b515f35dbe54955bf6e7e545
Admin <cypher.is.3301@gmail.com>
2021-05-24 18:22:50 +0300

    service: Service Removed registerPackage(), added saveClientsPackages(.).

--- src/main/java/com/example/demo/service/Service.java
+++ src/main/java/com/example/demo/service/Service.java
@@ -1,6 +1,8 @@
 package com.example.demo.service;
 
+import com.example.demo.data.ClientsPackagesId;
 import com.example.demo.dto.ClientDto;
+import com.example.demo.dto.ClientsPackagesDto;
 import com.example.demo.dto.OperatorDto;
 import com.example.demo.dto.PackageDto;
 import com.example.demo.entity.Package;
@@ -12,10 +14,13 @@ public interface Service {
     ClientDto saveClient(ClientDto clientDto);
     PackageDto savePackage(PackageDto packageDto);
     OperatorDto saveOperator(OperatorDto operatorDto);
+    ClientsPackagesDto saveClientsPackages(ClientsPackagesId clientsPackagesId);
+
+
+
 
     List<PackageDto> findAllPackages();
     List<OperatorDto> findAllOperators();
     List<ClientDto> findAllClients();
 
-    PackageDto registerPackage(ClientDto sender, ClientDto recipient, PackageDto pack) throws ValidationException;
 }
