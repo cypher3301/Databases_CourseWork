2bc385acd227dfa04dcd1876ae20327e4b7421a9
Admin <cypher.is.3301@gmail.com>
2021-05-27 18:14:37 +0300

    controller: PackageController Added Rest: /findClientById - pass "id" using GET query type, success:id. /closeInvoicePackage - pass "id" using GET query type, success PackageDto. Changed: /regPack - now has a timer. /createPackage - minor change
    /createClient->/createClientSender(address is default),/createClientRecipient.

--- src/main/java/com/example/demo/controller/PackageController.java
+++ src/main/java/com/example/demo/controller/PackageController.java
@@ -5,19 +5,16 @@ import com.example.demo.dto.ClientDto;
 import com.example.demo.dto.ClientsPackagesDto;
 import com.example.demo.dto.OperatorDto;
 import com.example.demo.dto.PackageDto;
-import com.example.demo.entity.Client;
-import com.example.demo.entity.ClientsPackages;
-import com.example.demo.entity.Package;
 import com.example.demo.service.Service;
-import com.sun.xml.bind.v2.runtime.reflect.Lister;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import javax.xml.bind.ValidationException;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @RestController
 @RequestMapping("/package")
@@ -27,24 +24,65 @@ import java.util.List;
 public class PackageController {
     private final Service service;
 
+    @Autowired
+    public PackageController(Service service) {
+        this.service = service;
+    }
 
-    @PostMapping("/createClient")
-    public Long registerClient(@RequestBody ClientDto clientDto){
+    private long recipient=0;
+    private long sender=0;
+    private long pack=0;
+
+
+    @PostMapping("/createClientSender")
+    public Long registerClientSender(@RequestBody ClientDto clientDto){
         log.info("Handling save client: " + clientDto);
-        return service.saveClient(clientDto).getId();
+        long id = service.saveClient(clientDto).getId();
+        sender = id;
+        return id;
+    }
+
+    @PostMapping("/createClientRecipient")
+    public Long registerClientRecipient(@RequestBody ClientDto clientDto){
+        log.info("Handling save client: " + clientDto);
+        long id = service.saveClient(clientDto).getId();
+        recipient=id;
+        return id;
     }
 
     @PostMapping("/createPackage")
     public Long registerPackage(@RequestBody PackageDto packageDto){
         log.info("Handling save package: " + packageDto);
-        return service.savePackage(packageDto).getId();
+        long id = service.savePackage(packageDto).getId();
+        pack=id;
+        return id;
     }
 
     @PostMapping("/regPack")
     public ClientsPackagesDto registerPackage(@RequestBody ClientsPackagesId clientsPackages){
+        while (sender == 0 || recipient == 0 || pack == 0){
+            Timer timer = new Timer();
+            timer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+
+                }
+            },500);
+        }
+        clientsPackages.setSenderId(sender);
+        clientsPackages.setRecipientId(recipient);
+        clientsPackages.setPackageId(pack);
         ClientsPackagesDto clientsPackagesDto = service.saveClientsPackages(clientsPackages);
         log.info("Handling save clients-packages: "+clientsPackagesDto);
+
+        resetIds();
         return clientsPackagesDto;
+    }
+
+    private void resetIds(){
+        this.pack=0;
+        this.sender=0;
+        this.recipient=0;
     }
 
     @GetMapping("/findAllOperators")
@@ -64,6 +102,17 @@ public class PackageController {
     public List<ClientDto> findAllClients() {
         log.info("Handling find all packages request");
         return service.findAllClients();
+    }
+
+    @GetMapping("/findClientById")
+    public ClientDto findClientById(@RequestParam Long id) {
+        return service.findClientById(id);
+    }
+
+    @GetMapping("/closeInvoicePackage")
+    public PackageDto closePackage(@RequestParam Long id){
+        log.info("Handling close package: " + id);
+        return service.closePackage(id);
     }
 
 }
