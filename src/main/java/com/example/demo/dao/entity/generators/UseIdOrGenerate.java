9b371fc88e38091f154b9d7a10b38c5f85278780
Admin <cypher.is.3301@gmail.com>
2021-08-12 13:58:49 +0300

    added ids generator

---
+++ src/main/java/com/example/demo/dao/entity/generators/UseIdOrGenerate.java
@@ -0,0 +1,23 @@
+package com.example.demo.dao.entity.generators;
+
+import com.example.demo.dao.entity.ancestor.BaseEntity;
+import com.example.demo.dao.entity.ancestor.Person;
+import org.hibernate.HibernateException;
+import org.hibernate.engine.spi.SharedSessionContractImplementor;
+import org.hibernate.id.IdentityGenerator;
+
+import java.io.Serializable;
+
+public class UseIdOrGenerate extends IdentityGenerator {
+
+    @Override
+    public Serializable generate(SharedSessionContractImplementor s, Object obj) {
+        if (obj == null){ throw new HibernateException(new NullPointerException());}
+
+        if (((BaseEntity) obj).getId() == null) {
+            return super.generate(s, obj);
+        }
+        return ((BaseEntity) obj).getId();
+
+    }
+}
