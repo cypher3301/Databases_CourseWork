027d3bfff761e59b22232e189ded9eb849f4d828
cypher3301 <koshulkov@gmail.com>
2021-12-02 12:35:49 +0200

    @CrossOrigin fix

--- src/main/java/com/spring/post/controller/EmptyWorkShiftController.java
+++ src/main/java/com/spring/post/controller/EmptyWorkShiftController.java
@@ -3,11 +3,13 @@ package com.spring.post.controller;
 import com.spring.post.entity.Users;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/empty")
+@CrossOrigin
 public class EmptyWorkShiftController {
 
     private static Users user;
--- src/main/java/com/spring/post/controller/OpenWorkShiftController.java
+++ src/main/java/com/spring/post/controller/OpenWorkShiftController.java
@@ -1,11 +1,13 @@
 package com.spring.post.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
+@CrossOrigin
 public class OpenWorkShiftController {
 
     @GetMapping("/to-open")
